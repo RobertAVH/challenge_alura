d8fe221f2d28d71b9f6075e3f8051d12dc911c0b
Roberto Valdez <robert.avaldezh@gmail.com>
2025-02-05 12:35:31 -0600

    Se agrega funcionalisas JS y readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Amigo Secreto
+
+## Descripción
+Este proyecto es una aplicación para organizar un juego de Amigo Secreto. Permite a los usuarios registrarse, crear grupos y asignar aleatoriamente a cada participante un amigo secreto.
+
+## Características
+- Registro de usuarios
+- Asignación aleatoria de amigos secretos
+
+## Uso
+1. Escribir un nombre
+2. Click en añadir
+3. Se muestran los usuarios agregados
+4. Click en Sortear Amigo
+5. Se muesta el amigo sorteado
+https://drive.google.com/file/d/13PSuMP1vVOG8xm-S6bR0b-ZHU17Vx6iF/view?usp=sharing
+
+## Licencia
+Este proyecto está bajo la Licencia MIT. Consulta el archivo [LICENSE](LICENSE) para más detalles.
--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-a
+let amigos = []
+
+function agregarAmigo() {
+    nombre = document.getElementById('amigo').value
+    if (nombre == "") {
+        alert('Debe ingresar un nombre')
+        return
+    } else {
+        amigos.push(nombre)
+        document.getElementById('amigo').value = ""
+        mostrarAmigos()
+        console.log(amigos)
+    }
+}
+
+function mostrarAmigos() {
+    document.getElementById('listaAmigos').innerHTML = "<li></li>"
+    for (let i = 0; i < amigos.length; i++) {
+        document.getElementById('listaAmigos').innerHTML += '<li>'+ amigos[i] + '</li>'
+    }
+}
+
+function sortearAmigo() {
+    document.getElementById('listaAmigos').innerHTML = "<li></li>"
+    let random = Math.floor(Math.random() * amigos.length)
+    document.getElementById('resultado').innerHTML = amigos[random]
+}
